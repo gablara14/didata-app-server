056d741b4aa4241dcf39bf28af488c042570f2a7
gabriel <gabrielr.lara@hotmail.com>
2021-05-22 12:11:23 -0300

    pc format

--- controllers/InteractionController.js
+++ controllers/InteractionController.js
@@ -1,16 +1,32 @@
 
 const mongoose = require('mongoose')
 const Publication = mongoose.model('Publication')
+const Interaction = mongoose.model('Interaction')
 
 
 
 class InteractionController {
 
-    static async patchInteraction(req,res) {
-        const { like } = req.body
-        const { id } = req.params
+    static async createInteraction(req,res) {
+        const { userId } = req.body
+        const { publicationId } = req.params
+
         try{
-            const publication = await Publication.findOneAndUpdate({ _id: id },{ $inc: { 'likes': like }},{ new:true })
+            const interaction = new Interaction({userId, publicationId})
+            interaction.save()
+            const publication = await Publication.findOneAndUpdate({ _id: id },{ $inc: { 'likes': 1 }},{ new:true })
+            return res.status(200).send(publication)
+        }catch(err){
+            return res.status(422).send(err.message)
+        }
+    }
+    
+    static async deleteInteraction(req,res) {
+        const { userId } = req.body
+        const { publicationId } = req.params
+        try{
+            await Interaction.findOneAndDelete({userId, publicationId})
+            const publication = await Publication.findOneAndUpdate({ _id: id },{ $inc: { 'likes': -1 }},{ new:true })
             return res.status(200).send(publication)
         }catch(err){
             return res.status(422).send(err.message)
--- routes/interactionRoutes.js
+++ routes/interactionRoutes.js
@@ -2,7 +2,10 @@ const InteractionController = require('../controllers/InteractionController')
 const express = require('express')
 const router = express.Router()
 
-router.patch('/publications/:id/interactions', InteractionController.patchInteraction)
+router.post('/publications/:id/interactions', InteractionController.createInteraction)
+
+router.delete('/publications/:id/interactions', InteractionController.deleteInteraction)
+
 
 
 module.exports = router
